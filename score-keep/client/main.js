5c32049c6ba25fc465038f4824d833a2fce2bac5
Jeane Carlos <volbohel@gmail.com>
2017-09-10 16:10:14 -0400

    Finished Most Functionalities

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,10 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 
-Meteor.startup(function () {
-  let name = 'John';
+import { Players } from '../imports/api/players';
 
-  let jsx = <p>Hello {name}</p>
-  ReactDOM.render(jsx, document.getElementById('app'));
+
+const renderPlayers = (playersList) => {
+
+  console.log(playersList)
+
+  return newNumbers = playersList.map( player => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button onClick={() => {
+          Players.update({_id: player._id}, {
+            $inc: {score: -1}
+          });
+        }}>-1</button>
+        <button onClick={() => {
+          Players.update({_id: player._id}, {
+            $inc: {score: 1}
+          });
+        }}>+1</button>
+        <button onClick={() => {
+            Players.remove({_id: player._id});
+          }}>X</button>
+      </p>
+  )
+  });
+
+}
+
+const handleSubmit = (e) => {
+  let { value } = e.target.playerName
+
+  e.preventDefault(); //Stops page freshing
+
+  if (value) {
+    // Insert new doc into client collection
+    Players.insert({
+      name: value,
+      score: 0
+    });
+
+    e.target.playerName.value = '';
+  }
+};
+
+Meteor.startup(() => {
+
+
+  Tracker.autorun( () => {
+    let players = Players.find().fetch();
+    let name = 'John';
+    let title = 'Score Keep'
+
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}</p>
+      {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+        <button>Add Player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+
+
+
 });
---
+++ score-keep/server/main.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import { Players } from '../imports/api/players';
+
+Meteor.startup( () => {
+
+  // let square = function (x, y) {
+  //   return x * x
+  // }
+
+
+});
